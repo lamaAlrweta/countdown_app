35a004de2f8992e1c84f468d7eaabc8beee2026e
AfafYa20 <Afaf.ya27@gmail.com>
2021-09-13 21:23:38 +0300

    Create card component

--- lib/main.dart
+++ lib/main.dart
@@ -13,20 +13,20 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.white,
-        primaryColor: Color.fromRGBO(113, 90, 206, 1),
-        backgroundColor: Color.fromRGBO(0, 22, 30, 41),
-        accentColor: Color.fromRGBO(75, 83, 228, 1),
-        // accentColorBrightness: Brightness.dark,
-        buttonTheme: ButtonTheme.of(context).copyWith(
-          buttonColor: Color.fromRGBO(75, 83, 228, 0.3),
-          textTheme: ButtonTextTheme.primary,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20),
-          ),
-        ),
-      ),
+      // theme: ThemeData(
+      //   primarySwatch: Colors.white,
+      //   primaryColor: Color.fromRGBO(113, 90, 206, 1),
+      //   backgroundColor: Color.fromRGBO(0, 22, 30, 41),
+      //   accentColor: Color.fromRGBO(75, 83, 228, 1),
+      //   // accentColorBrightness: Brightness.dark,
+      //   buttonTheme: ButtonTheme.of(context).copyWith(
+      //     buttonColor: Color.fromRGBO(75, 83, 228, 0.3),
+      //     textTheme: ButtonTextTheme.primary,
+      //     shape: RoundedRectangleBorder(
+      //       borderRadius: BorderRadius.circular(20),
+      //     ),
+      //   ),
+      // ),
       home: HomeScreen(),
     );
   }
--- lib/widgets/countdown_card.dart
+++ lib/widgets/countdown_card.dart
@@ -1,13 +1,187 @@
 import 'package:flutter/material.dart';
 
 class CountdaownCard extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 123 ,
-      color: Colors.amber,
-      child: Text('hi',style: TextStyle(fontSize: 16),),
+    return Card(
+      child: ClipRRect(
+        // borderRadius: BorderRadius.all(
+        //   Radius.circular(12),
+        // ),
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: 132,
+          margin: EdgeInsets.all(16),
+          //padding: EdgeInsets.all(12),
+
+          color: Colors.amber,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 12, top: 12, right: 12, bottom: 12),
+                child: Row(
+                  children: [
+                    Container(
+                      // alignment: Alignment.topLeft,
+                      color: Colors.black,
+                      width: 46,
+                      height: 46,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'My birthday',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            'Sep 19, 2021',
+                            style: TextStyle(
+                              fontSize: 14,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  color: Colors.blueAccent,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 8, right: 8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      //crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                                // border: Border.all(width: 2),
+                                ),
+                            padding: EdgeInsets.all(2),
+                            child: Column(
+                              children: [
+                                Text(
+                                  '14',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  'Days',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                                // border: Border.all(width: 2),
+                                ),
+                            padding: EdgeInsets.all(2),
+                            child: Column(
+                              children: [
+                                Text(
+                                  '14',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  'Days',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                                // border: Border.all(width: 2),
+                                ),
+                            padding: EdgeInsets.all(2),
+                            child: Column(
+                              children: [
+                                Text(
+                                  '14',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  'Days',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                                // border: Border.all(width: 2),
+                                ),
+                            padding: EdgeInsets.all(2),
+                            child: Column(
+                              children: [
+                                Text(
+                                  '14',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  'Days',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
+    // return Container(
+    //   height: 123 ,
+    //   width: 327,
+    //   color: Colors.amber,
+    //   child: Text('hi',style: TextStyle(fontSize: 16),),
+    // );
   }
 }
