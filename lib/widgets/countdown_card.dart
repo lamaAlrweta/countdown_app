2dca3cd1ac6708bba25a0c257a4725cb98b09f67
AfafYa20 <Afaf.ya27@gmail.com>
2021-09-13 18:03:44 +0300

    Create new screens and wigtdgets folders

---
+++ lib/widgets/countdown_card.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CountdaownCard extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 123 ,
+      color: Colors.amber,
+      child: Text('hi',style: TextStyle(fontSize: 16),),
+    );
+  }
+}
